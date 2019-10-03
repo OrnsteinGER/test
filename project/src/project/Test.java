11a75857dffdf8efc45e30ed3ebf075feb8a4429
Martin Gross <martin.gross.lev.de>
2019-10-03 22:34:34 +0200

    üromhed

--- project/src/project/Test.java
+++ project/src/project/Test.java
@@ -4,7 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		
+		
 
+	}
+	
+	public static void printName(String name) {
+		System.out.println(name);
 	}
 
 }
