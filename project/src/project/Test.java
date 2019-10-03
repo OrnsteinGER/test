82db9806aaedd7decc7d705cf125e304c95cb0d5
Martin Gross <martin.gross.lev.de>
2019-10-03 22:17:42 +0200

    commit

--- project/src/project/Test.java
+++ project/src/project/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("development");
+<<<<<<< Upstream, based on origin/development
+=======
+		System.out.println("blub");
+>>>>>>> 56fddef commit
 
 	}
 
