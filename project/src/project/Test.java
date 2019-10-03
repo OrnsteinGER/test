7bb9d172fb6f6a769e204f67fd4963090f43bc37
Martin Gross <martin.gross.lev.de>
2019-10-03 22:54:43 +0200

    aefcwef

--- project/src/project/Test.java
+++ project/src/project/Test.java
@@ -4,12 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-<<<<<<< HEAD
-		System.out.println("development");
-		System.out.println("development");
-=======
 		
->>>>>>> refs/heads/master
+		
+		
 		
 
 	}
